82232aceef8f8338e999cf2eca1f2044b5bbe4f5
Gunnar Bastkowski <gunnar@digitalstep.de>
2013-09-25 13:37:09 +0200

    Migrate starter class to Scala

---
+++ src/main/scala/de/digitalstep/ntlmproxy/NtlmProxy.scala
@@ -0,0 +1,27 @@
+package de.digitalstep.ntlmproxy
+
+import com.google.common.base.Optional
+import de.digitalstep.ntlmproxy.ui.TrayIconBuilder
+import java.net.ServerSocket
+import com.typesafe.scalalogging.slf4j.Logging
+import java.util.concurrent.Executors
+
+object NtlmProxy extends Logging {
+	
+	val EXECUTOR = Executors.newCachedThreadPool
+
+	def main(args: Array[String]) {
+		System.setProperty("java.net.useSystemProxies", "true")
+
+		val listener = new TrayIconBuilder().build()
+		val port = Integer.parseInt(args(0))
+		val server = new ServerSocket(port)
+		logger.info(s"Proxy listening on port ${port}")
+		while (true) {
+			val socket = server.accept()
+			EXECUTOR execute new Handler(socket, listener)
+		}
+		
+	}
+
+}
